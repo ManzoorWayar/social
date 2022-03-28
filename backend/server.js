2a83a412edd417f86e175a7688f53391999909df
manzoor wayar <softwareengineerwayar@gmail.com>
2022-03-28 16:37:43 +0430

    initial app setup

---
+++ backend/server.js
@@ -0,0 +1,70 @@
+import path from "path";
+import express from "express";
+import color from "colors";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import mongoSanitize from "express-mongo-sanitize";
+import helmet from "helmet";
+import xss from "xss-clean";
+import hpp from "hpp";
+import cors from "cors";
+import limiter from "./utils/limiter.js";
+import connectDB from "./config/db.js";
+import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
+
+// Load env vars
+dotenv.config();
+
+// Connect to database
+connectDB();
+
+// Route files
+
+const app = express();
+
+// Body parser
+app.use(express.json());
+
+// Nested objects
+app.use(express.urlencoded({ extended: true }));
+
+// Cookie parser
+app.use(cookieParser());
+
+// Sanitize data
+app.use(mongoSanitize());
+
+// Set security headers
+app.use(helmet());
+
+// Prevent XSS attacks
+app.use(xss());
+
+// Rate limiting
+app.use(limiter);
+
+// Prevent http param pollution
+app.use(hpp());
+
+// Enable CORS
+app.use(cors());
+
+// Mount routers
+
+const __dirname = path.resolve();
+app.use(
+  "/public/uploads",
+  express.static(path.join(__dirname, "/public/uploads"))
+);
+
+// Error Handling Middlewares
+app.use(notFound);
+app.use(errorHandler);
+
+const PORT = process.env.PORT | 5005;
+
+app.listen(PORT, () =>
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
+);
