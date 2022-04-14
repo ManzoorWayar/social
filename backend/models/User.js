91d3f1a342496ef3ed390063ee233b0565ee0ddf
manzoor wayar <softwareengineerwayar@gmail.com>
2022-04-14 14:11:53 +0430

    add index to email

--- backend/models/User.js
+++ backend/models/User.js
@@ -15,7 +15,9 @@ const UserSchema = new mongoose.Schema(
     },
     email: {
       type: String,
-      unique: true,
+      index: {
+        unique: true,
+      },
       match: [
         /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
         "Please add a valid email",
